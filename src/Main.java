cd00698ff75d9608c378b5ea96cfa31640e3d2aa
emreerkus <emre.erkus@ozu.edu.tr>
2023-12-14 00:13:55 +0300

    second commit feature-2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("feature-2");
+        System.out.println("feature-2 2");
     }
 }
